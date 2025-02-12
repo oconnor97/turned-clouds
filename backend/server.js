fa07ba7976ecacfb1eb66a93fd8a4669fd255b14
Michael O'Connor <oconnor97@aol.com>
2025-02-11 20:33:35 -0500

    setting up the root routes to go through show.route.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 
-
 // Allows you to access your env file
 dotenv.config();
 
@@ -10,6 +9,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // allows us to use json in our req.body -- Middleware
+
+app.use('/api/shows', showRoutes) //going to use this now instead of including it in all of the routes in show.route.js
+// Server.js hits show.route.js then hits show.conrtroller.js to house all of the functionality in one place
 
 app.listen(PORT, () => {
     connectDB();
