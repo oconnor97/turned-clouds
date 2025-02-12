a1503042a522a1742f7625499701daa35fab3a19
Michael O'Connor <oconnor97@aol.com>
2025-02-11 20:30:28 -0500

    adding middleware to server.js

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); // allows us to use json in our req.body -- Middleware
+
 app.listen(PORT, () => {
     connectDB();
     console.log(`Server started at http://localhost:${PORT}`);
