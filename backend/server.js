b75b0cb08d7fa35a68bab82deaf729fcea8d91b0
Michael O'Connor <oconnor97@aol.com>
2025-02-11 20:22:30 -0500

    creating test connection to MongoDB - Succesfull

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+
+// Allows you to access your env file
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server started at http://localhost:${PORT}`);
+});
